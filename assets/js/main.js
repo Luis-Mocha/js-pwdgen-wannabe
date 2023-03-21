93cb9133e30c1e155f461999d559359474cf7b01
Luis-Mocha <luismocha97@gmail.com>
2023-03-21 17:33:26 +0100

    Javascript "shuffle" info / reset button

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,21 +6,42 @@ const form = document.querySelector('#form')
 let newPasswordDiv = document.querySelector('#new-password')
 
 form.addEventListener('submit', function (invioForm) {
-
+    //Evito che si ricarichi il sito
     invioForm.preventDefault();
 
-    const name = formName.value;
+    // valori input form
+    const nameForm = formName.value;
     const surname = formSurname.value;
     const color = formColor.value;
 
-    //let infoPassword = {name, surname, color}
-    //console.log(infoPassword)
+    let infoPassword = [nameForm, surname, color]
+    console.log(infoPassword)
 
+    let shuffledInfo = infoPassword.sort(function () {
+        return Math.random() - 0.5;
+    });
+
+    console.log(shuffledInfo)
+
+    // funzione per innerHTML
+    function generatePassword( ) {
     newPasswordDiv.innerHTML =
     `
     <span class="fs-3">La tua nuova password è:</span>
-    <h2>${name}${surname}${color}</h2>
+    <h2>${shuffledInfo.join('')}</h2>
     `
+    }
 
-    form.reset ();
+    generatePassword();
+
 });
+
+
+// Tasto per resettare il form
+
+const resetButton = document.querySelector('#reset-button')
+
+resetButton.addEventListener('click', function resetFunction(params) {
+    form.reset ();
+})
+
